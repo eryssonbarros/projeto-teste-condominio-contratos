8ca925d18a2addad0001dfa7839e10ffebe06cb7
Carlos H. Campello <carlos@qualinfo.com.br>
2023-01-24 08:04:26 -0300

    Script de deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,17 @@
 const hre = require("hardhat");
 
 async function main() {
+  const Condominio = await hre.ethers.getContractFactory("Condominio");
+  const condominio = await Condominio.deploy();
+  await condominio.deployed();
+
+  console.log("Condomínio implantado no endereço " + condominio.address);
+
+  const Pleito = await hre.ethers.getContractFactory("Pleitos");
+  const pleitos = await Pleito.deploy(condominio.address);
+  await pleitos.deployed();
+
+  console.log("Pleitos implantado no endereço " + pleitos.address);
 }
 
 main().catch((error) => {
